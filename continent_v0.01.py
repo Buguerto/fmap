c6fba25f3e640f466f499498a3f65c8175011d1b
Buguerto <102261596+Buguerto@users.noreply.github.com>
2022-06-10 18:51:26 -0500

    Add files via upload
    Continent_ module generates a landmass from a chain of connected dots.

---
+++ continent_v0.01.py
@@ -0,0 +1,68 @@
+import numpy as np
+from PIL import Image, ImageDraw
+from PIL import ImageFilter
+
+# Patron, los puntos deben moverse solo un lugar en una direccion a la vez.
+#lista_patron = [1, 0, -1]
+# Las seeds se generan invocando un numero aleatorio del patron cada vez que el codigo rota.
+#seedx = (lista_patron[np.random.randint(0,3)])
+#seedy = (lista_patron[np.random.randint(0,3)])
+#seed = (seedx, seedy)
+#print(seed)
+# La coordenada inicial queda determinada una sola vez, es coordinate_f. A partir de esa coordenada se aplica el patron
+#coordinate_f = (np.random.randint(1, 1000),np.random.randint(1, 1000))
+#print(coordinate_f)
+#x = coordinate_f[0]+seedx
+#y = coordinate_f[1]+seedy
+#print(x)
+#print(y)
+#
+
+w, h= 1000, 1000
+fmap= Image.new("RGB", (w,h), ("Black"))
+draw = ImageDraw.Draw(fmap)
+npuntos = 0
+while npuntos < 20000:
+    lista_patron = [-1, 0, 1]
+    seedx = (lista_patron[np.random.randint(0,3)])
+    seedy = (lista_patron[np.random.randint(0,3)])
+    coordinate_f = (500,500)
+    seed = (seedx, seedy)
+    difx = 0
+    dify = 0
+    if npuntos == 0:
+        x = coordinate_f[0]+seedx
+        y = coordinate_f[1]+seedy
+        coordinate_n = (x,y)
+        print('este es coordinate_n')
+        print(coordinate_n)
+        npuntos = npuntos + 1
+    while npuntos != 300000:
+        seedx_1 = (lista_patron[np.random.randint(0,3)])
+        seedy_1 = (lista_patron[np.random.randint(0,3)])
+        contador = npuntos 
+        x1 = x + seedx_1 + difx
+        y1 = y + seedy_1 + dify
+        coordinate_n1 = (x1,y1)
+        npuntos = npuntos + 1
+        draw.point((coordinate_n1), "Green")
+        difx = difx + lista_patron[np.random.randint(0,3)]
+        dify = dify + lista_patron[np.random.randint(0,3)]
+    if npuntos == 300000:
+        fmap.show()
+        fmap = fmap.filter(ImageFilter.EDGE_ENHANCE)
+        fmap.show()
+        fmap = fmap.filter(ImageFilter.CONTOUR)
+        fmap.show()
+        fmap = fmap.filter(ImageFilter.CONTOUR)
+        fmap.show()
+        fmap = fmap.filter(ImageFilter.CONTOUR)
+        fmap.show()
+        fmap = fmap.filter(ImageFilter.CONTOUR)
+        fmap.show()
+        
+
+    
+
+
+
